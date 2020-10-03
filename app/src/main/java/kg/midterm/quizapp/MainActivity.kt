8e9566a6559e47805238b9aadd15e9bf9e7caae5
Zaiyr <zaiyr.00@gmail.com>
2020-10-03 14:48:58 +0600

    added quiz activity

--- app/src/main/java/kg/midterm/quizapp/MainActivity.kt
+++ app/src/main/java/kg/midterm/quizapp/MainActivity.kt
@@ -1,11 +1,33 @@
 package kg.midterm.quizapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.id.button2
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    private var bestScore: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        showBestScoreOfUser()
+
+        buttonStartQuiz.setOnClickListener { view ->
+            val intent = Intent(view.context, QuizActivity::class.java)
+            view.context.startActivity(intent)
+        }
     }
+
+    private fun showBestScoreOfUser(){
+        textViewBestScore.text = "Your best score is: $bestScore"
+    }
+
 }
+
+
